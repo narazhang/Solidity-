3a76acb607d2ed2e32a68c7aa17e36c5a0e5833f
narazhang <740678704@qq.com>
2025-07-10 14:58:41 +0800

    Update solidity代码学习.sol

--- solidity代码学习.sol
+++ solidity代码学习.sol
@@ -1,6 +1,36 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity 0.8.19;
 
-contract Counter {
+contract SimpleStorage{
+
+    uint256  public  favoriteNumber;
+
+    //uint256[] listofFavoriteNumbers;
+
+    struct Person{
+        uint256 myFavoriteNumber;
+        string name;
+
+    }
+
+    Person[] public listOfPeople;
+
+    // Person public myFriend = Person( 1, "john");
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+        favoriteNumber = favoriteNumber + 1;
+        
+    }
+
+    function retrieve() private  view   returns (uint256){
+        return favoriteNumber;
+    }
     
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        //Person memory newPerson = Person(_favoriteNumber,_name);
+        listOfPeople.push(Person(_favoriteNumber, _name));
+
+    }
+
 }
